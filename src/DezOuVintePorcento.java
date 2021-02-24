0e27c13487881074227ed8091d2049043869bb18
Allan <allanborges20000@gmail.com>
2021-02-24 19:34:33 -0300

    Solid

---
+++ src/DezOuVintePorcento.java
@@ -0,0 +1,10 @@
+
+
+public class DezOuVintePorcento implements RegraDeCalculo{
+
+	@Override
+	public double calcula(Funcionario funcionario) {
+		return 0;
+	}
+
+}
---
+++ src/QuinzeOuVintePorcento.java
@@ -0,0 +1,9 @@
+
+public class QuinzeOuVintePorcento implements RegraDeCalculo{
+
+	@Override
+	public double calcula(Funcionario funcionario) {
+		return 0;
+	}
+
+}
